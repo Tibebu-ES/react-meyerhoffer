f16b1323a765513ffbb4b593785d5d00739c0308
Tibebu-ES <tibebuenyew@gmail.com>
2021-07-09 20:24:53 +0300

    Project setup

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The new meyerhoffer site is underconstraction.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://meyerhoffer.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Old website
         </a>
       </header>
     </div>
