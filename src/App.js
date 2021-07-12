fddf902d3575e0769bd9bf91c232fcf5314588de
Tibebu-ES <tibebuenyew@gmail.com>
2021-07-12 18:19:29 +0300

    init the website skeleton; Header, Navbar, Footer, Home, About, and Objects components are added; Routing is also added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/Home";
+import About from "./components/About";
+import Objects from "./components/Objects";
+import { Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The new meyerhoffer site is underconstraction.
-        </p>
-        <a
-          className="App-link"
-          href="https://meyerhoffer.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Old website
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/archive" component={Objects} />
+      <Footer />
+    </>
   );
 }
 
