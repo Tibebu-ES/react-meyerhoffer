77fa451881cae5c6b43b44668069fbec3ab30f0e
Tibebu-ES <tibebuenyew@gmail.com>
2021-07-13 21:09:15 +0300

    Navbar is build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,82 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { AppBar, Toolbar, makeStyles, Button } from "@material-ui/core";
+import { useHistory } from "react-router";
 import Header from "./Header";
 
+const useStyles = makeStyles((theme) => ({
+  alignItemsAndJustifyContent: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  toolbarItem: {
+    marginRight: "2rem",
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+  selectedToolbarItem: {
+    background: "#FFFFFF",
+    color: "#3f51b5",
+    "&:hover": {
+      background: "#FFFFFF",
+      color: "#3f51b5",
+    },
+  },
+}));
+
 const Navbar = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const [selectedPage, setSelectedPage] = useState("home");
+  const toolbarItemClicked = (event, page) => {
+    event.preventDefault();
+    if (page !== selectedPage) {
+      const item = document.querySelector("#" + page);
+      const previousItem = document.querySelector("#" + selectedPage);
+      item.classList.add(classes.selectedToolbarItem);
+      previousItem.classList.remove(classes.selectedToolbarItem);
+      //set current page
+      setSelectedPage(page);
+    }
+    history.push("/" + page);
+  };
+
   return (
     <div>
       <div>
         <Header />
       </div>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">HOME</Link>
-          </li>
-          <li>
-            <Link to="/about">ABOUT</Link>
-          </li>
-          <li>
-            <Link to="/archive">OBJECTS</Link>
-          </li>
-        </ul>
-      </div>
+      <AppBar position="sticky">
+        <Toolbar className={classes.alignItemsAndJustifyContent}>
+          <Button
+            className={classes.selectedToolbarItem + " " + classes.toolbarItem}
+            color="inherit"
+            href="/"
+            onClick={(e) => toolbarItemClicked(e, "home")}
+            id="home"
+          >
+            Home
+          </Button>
+          <Button
+            className={classes.toolbarItem}
+            href="/about"
+            color="inherit"
+            onClick={(e) => toolbarItemClicked(e, "about")}
+            id="about"
+          >
+            About
+          </Button>
+          <Button
+            className={classes.toolbarItem}
+            href="/archive"
+            color="inherit"
+            onClick={(e) => toolbarItemClicked(e, "archive")}
+            id="archive"
+          >
+            OBJECTS
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
