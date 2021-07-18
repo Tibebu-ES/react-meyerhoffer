4eb4bd21290a00efe11318362a9a6d810a28cef7
Tibebu-ES <tibebuenyew@gmail.com>
2021-07-18 16:22:44 +0300

    Appbar is updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Home, About, Objects, Navbar, Footer } from "./components/index";
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/main/Header";
 
 function App() {
   return (
-    <>
+    <div>
+      <Header />
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/archive" component={Objects} />
       </Switch>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -1,99 +1,71 @@
-import React, { useState, useEffect } from "react";
-import { AppBar, Toolbar, makeStyles, Button } from "@material-ui/core";
-import { useHistory } from "react-router";
-import Header from "./Header";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  Grid,
+  Typography,
+  AppBar,
+  Toolbar,
+  makeStyles,
+  IconButton,
+} from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
-  alignItemsAndJustifyContent: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  toolbarItem: {
-    marginRight: "2rem",
-    "&:hover": {
-      textDecoration: "underline",
-    },
-  },
-  selectedToolbarItem: {
-    background: "#FFFFFF",
-    color: "#3f51b5",
-    "&:hover": {
-      background: "#FFFFFF",
-      color: "#3f51b5",
-    },
+  nav_link: {
+    fontFamily: "Century",
+    color: "#fffefe",
+    textDecoration: "none",
+    color: "#fffefe",
+    textDecoration: "none",
   },
 }));
 
 const Navbar = (props) => {
   const classes = useStyles();
-  const history = useHistory();
-
-  const [selectedPage, setSelectedPage] = useState("home");
-  const [appbarPosition, setAppbarPosition] = useState("static");
-
-  const toolbarItemClicked = (event, page) => {
-    event.preventDefault();
-    if (page !== selectedPage) {
-      const item = document.querySelector("#" + page);
-      const previousItem = document.querySelector("#" + selectedPage);
-      item.classList.add(classes.selectedToolbarItem);
-      previousItem.classList.remove(classes.selectedToolbarItem);
-      //set current page
-      setSelectedPage(page);
-    }
-    history.push("/" + page);
-    window.scrollTo(0, 0);
-  };
-
-  //
-  useEffect(() => {
-    //make navbar fixed on scroll
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > 70) {
-        setAppbarPosition("fixed");
-      } else {
-        setAppbarPosition("static");
-      }
-    });
-  }, []);
 
   return (
-    <div>
-      <div>
-        <Header />
-      </div>
-      <AppBar id="appbar" position={appbarPosition}>
-        <Toolbar className={classes.alignItemsAndJustifyContent}>
-          <Button
-            className={classes.selectedToolbarItem + " " + classes.toolbarItem}
-            color="inherit"
-            href="/"
-            onClick={(e) => toolbarItemClicked(e, "home")}
-            id="home"
-          >
-            Home
-          </Button>
-          <Button
-            className={classes.toolbarItem}
-            href="/about"
-            color="inherit"
-            onClick={(e) => toolbarItemClicked(e, "about")}
-            id="about"
-          >
-            About
-          </Button>
-          <Button
-            className={classes.toolbarItem}
-            href="/archive"
-            color="inherit"
-            onClick={(e) => toolbarItemClicked(e, "archive")}
-            id="archive"
-          >
-            OBJECTS
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="sticky">
+      <Toolbar variant="dense">
+        <Grid container spacing={3} alignItems="center" justify="center">
+          <Grid item>
+            <Typography variant="h6">
+              <NavLink
+                exact
+                to="/"
+                className={classes.nav_link}
+                activeStyle={{ color: "aqua" }}
+              >
+                HOME
+              </NavLink>
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h6">
+              <NavLink
+                exact
+                to="/about"
+                className={classes.nav_link}
+                activeStyle={{ color: "aqua" }}
+              >
+                ABOUT
+              </NavLink>
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h6">
+              <NavLink
+                exact
+                to="/archive"
+                className={classes.nav_link}
+                activeStyle={{ color: "aqua" }}
+              >
+                OBJECTS
+              </NavLink>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 };
 
