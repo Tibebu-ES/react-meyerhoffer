4f921a26d492054e834a64b8ebfcb67a91f9f082
Tibebu-ES <tibebuenyew@gmail.com>
2021-07-18 18:11:31 +0300

    Put the 'i still use pen and pencil' image and surfing video in a row; add css style to quoted paragraph; update the surfers blood movie section

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -9,10 +9,13 @@ import {
   Container,
   Box,
   Grid,
+  Badge,
 } from "@material-ui/core";
 import { ExpandMore } from "@material-ui/icons";
 import ReactPlayer from "react-player";
 import { about_files } from "../data/index";
+import ReactAudioPlayer from "react-audio-player";
+import audio from "../../assets/dialog.ogg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,6 +40,15 @@ const useStyles = makeStyles((theme) => ({
   surfer_info: {
     margin: "50px",
   },
+  qoute: {
+    margin: "50px",
+    background: "#2E8BC0",
+  },
+  qoute_p: {
+    padding: "45px",
+    fontFamily: "Open Sans",
+    color: "#fff",
+  },
 }));
 
 const About = () => {
@@ -51,49 +63,70 @@ const About = () => {
     <Container>
       <Grid container spacing={2}>
         <Grid item md={12}>
-          <Paper style={{ height: "500px" }}>
-            <img
-              src={about_files[0]}
-              style={{ height: "100%", width: "100%" }}
-              alt="meyerhoffer.com"
-            />
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={6} md={5}>
+              <Paper style={{ height: "90%", width: "90%" }}>
+                <img
+                  src={about_files[0]}
+                  style={{ height: "100%", width: "100%" }}
+                  alt="meyerhoffer.com"
+                />
+              </Paper>
+            </Grid>
+            <Grid item xs={8} md={6}>
+              <ReactPlayer
+                playing={true}
+                loop={true}
+                url={about_files[1]}
+                style={{ height: "100%", width: "100%" }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item md={12} align="center">
+          <Paper className={classes.qoute}>
+            <Typography
+              variant="body1"
+              align="center"
+              className={classes.qoute_p}
+            >
+              "One time when I was in LA, I drove out to my old art school. It
+              was a Saturday, so there was almost nobody around. I stopped in
+              the art supplies store and bought some of my favorite items: some
+              tape rolls that we used draw with, some notebooks, and my favorite
+              pens. I began wondering how it was that I came to do this work,
+              and how I got through it the early phase of my career. I had these
+              tools, I’d learned a lot, and I had some vision about products.
+              But I didn’t yet have the experience needed to fully realize that
+              vision. When you have tools and vision, the central question
+              becomes “Where do you want to go?” Right now, I’m in a pretty
+              silent space, still trying to contemplate this. I strive to be
+              more intuitive, focusing on the present moment – that’s what’s
+              important. When opportunities arrive, I either say no, or I jump
+              in and focus on it."
+            </Typography>
           </Paper>
         </Grid>
-        <Grid item md={12}>
-          <ReactPlayer
-            playing={true}
-            loop={true}
-            url={about_files[1]}
-            width="100%"
-            height="100%"
-          />
-        </Grid>
-        <Grid item md={12} align="center">
-          <Typography variant="body1" align="center">
-            "One time when I was in LA, I drove out to my old art school. It was
-            a Saturday, so there was almost nobody around. I stopped in the art
-            supplies store and bought some of my favorite items: some tape rolls
-            that we used draw with, some notebooks, and my favorite pens. I
-            began wondering how it was that I came to do this work, and how I
-            got through it the early phase of my career. I had these tools, I’d
-            learned a lot, and I had some vision about products. But I didn’t
-            yet have the experience needed to fully realize that vision. When
-            you have tools and vision, the central question becomes “Where do
-            you want to go?” Right now, I’m in a pretty silent space, still
-            trying to contemplate this. I strive to be more intuitive, focusing
-            on the present moment – that’s what’s important. When opportunities
-            arrive, I either say no, or I jump in and focus on it."
-          </Typography>
-        </Grid>
+
         <Grid item md={12} className={classes.surfer_info}>
           <Typography variant="h6">SURFERS BLOOD</Typography>
           <Box display="flex" flexDirection="row">
             <Box md={6}>
-              <ReactPlayer playing={true} loop={true} url={about_files[1]} />
+              <ReactPlayer controls={true} url={about_files[4]} />
             </Box>
-            <Box md={6} style={{ marginLeft: "30px", marginTop: "80px" }}>
+            <Box md={6} style={{ marginLeft: "35px", marginTop: "80px" }}>
               <Typography variant="h6">Watch Surfers Blood movie</Typography>
-              <Typography variant="body1" paragraph>
+              <Typography
+                variant="body1"
+                paragraph
+                style={{ marginTop: "10px" }}
+              >
                 By renowned filmmaker Patrick Trefz explores the essence of the
                 sport he loves by profiling those who inspire him, from Basque
                 Country, Spain to Silicon Valley, USA (2016)
@@ -101,6 +134,7 @@ const About = () => {
             </Box>
           </Box>
         </Grid>
+
         <Grid item md={12}>
           <Paper style={{ height: "500px" }}>
             <img
