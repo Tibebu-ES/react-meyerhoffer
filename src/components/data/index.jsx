69659c109a46b7cfac324872ce1fbf78a074d5e2
Tibebu-ES <tibebuenyew@gmail.com>
2021-07-28 14:09:50 +0300

    Assets updated

--- src/components/data/index.jsx
+++ src/components/data/index.jsx
@@ -3,11 +3,11 @@ import h1 from "../../assets/images/1.jpg";
 import h2 from "../../assets/images/2.jpg";
 import h3 from "../../assets/images/3.jpg";
 import h4 from "../../assets/images/4.jpg";
-import h5 from "../../assets/videos/5.mp4";
+import h5 from "../../assets/images/5.gif";
 import h6 from "../../assets/images/6.jpg";
 import h7 from "../../assets/images/7.jpg";
 import h8 from "../../assets/images/8.jpg";
-import h9 from "../../assets/videos/9.mp4";
+import h9 from "../../assets/images/9.gif";
 import h10 from "../../assets/images/10.jpg";
 import h11 from "../../assets/images/11.jpg";
 import h12 from "../../assets/images/12.jpg";
@@ -25,7 +25,7 @@ import h23 from "../../assets/images/23.jpg";
 import h24 from "../../assets/images/24.jpg";
 import h25 from "../../assets/images/25.jpg";
 import h26 from "../../assets/images/26.jpg";
-import h27 from "../../assets/videos/27.mp4";
+import h27 from "../../assets/images/27.gif";
 import h28 from "../../assets/images/28.jpg";
 import h29 from "../../assets/images/29.jpg";
 import h30 from "../../assets/images/30.jpg";
@@ -38,123 +38,151 @@ import h36 from "../../assets/images/36.jpg";
 import h37 from "../../assets/images/37.jpg";
 import h38 from "../../assets/images/38.jpg";
 import h39 from "../../assets/images/39.jpg";
+import h40 from "../../assets/images/ob_link1/ob_sound1.jpg";
+import h41 from "../../assets/images/ob_link1/ob_sound2.jpg";
+import h42 from "../../assets/images/ob_link4/ob_charge1.jpg";
+import h43 from "../../assets/images/ob_link4/ob_charge2.jpg";
+import h44 from "../../assets/images/ob_link4/ob_charge3.jpg";
+import h45 from "../../assets/images/ob_link4/ob_charge4.jpg";
+import h46 from "../../assets/images/ob_link8/ob_cloth1.jpg";
+import h47 from "../../assets/images/ob_link8/ob_cloth2.jpg";
+import h48 from "../../assets/images/ob_link8/ob_cloth3.jpg";
+import h49 from "../../assets/images/ob_link8/ob_cloth4.jpg";
+import h50 from "../../assets/images/ob_link2/ob_glass1.jpg";
+import h51 from "../../assets/images/ob_link2/ob_glass2.jpg";
+import h52 from "../../assets/images/ob_link2/ob_glass3.jpg";
+import h53 from "../../assets/images/ob_link2/ob_glass4.jpg";
+import h54 from "../../assets/images/ob_link3/ob_logi1.jpg";
+import h55 from "../../assets/images/ob_link3/ob_logi2.jpg";
+import h56 from "../../assets/images/ob_link3/ob_logi3.jpg";
+import h57 from "../../assets/images/ob_link7/ob_neil1.jpg";
+import h58 from "../../assets/images/ob_link7/ob_neil2.jpg";
+import h59 from "../../assets/images/ob_link6/ob_danger1.jpg";
+import h60 from "../../assets/images/ob_link6/ob_danger2.jpg";
+import h61 from "../../assets/images/ob_link5/ob_cap1.jpg";
+import h62 from "../../assets/images/ob_link5/ob_cap2.jpg";
+import h63 from "../../assets/images/ob_link5/ob_cap3.jpg";
+import h64 from "../../assets/images/ob_link5/ob_cap4.jpg";
+import h65 from "../../assets/images/ob_link5/ob_cap5.jpg";
+import h66 from "../../assets/images/ob_link5/ob_cap6.jpg";
 
 // home component
-import c00 from "../../assets/videos/h2.mp4";
+import c00 from "../../assets/images/h2.gif";
 import c01 from "../../assets/images/h3.jpg";
-import c02 from "../../assets/videos/h4.mp4";
-import c03 from "../../assets/videos/h5.mp4";
-import c04 from "../../assets/videos/h6.mp4";
+import c02 from "../../assets/images/h4.gif";
+import c03 from "../../assets/images/h5.gif";
+import c04 from "../../assets/images/h6.gif";
 import c1 from "../../assets/images/c1.jpg";
 import c2 from "../../assets/images/c2.jpg";
 import c3 from "../../assets/images/c3.jpg";
 import c4 from "../../assets/images/c4.jpg";
 import c5 from "../../assets/images/c5.jpg";
-import c6 from "../../assets/hb_link1/blatch1.jpg";
-import c7 from "../../assets/hb_link1/blatch2.jpg";
-import c8 from "../../assets/hb_link1/blatch3.jpg";
-import c9 from "../../assets/hb_link1/blatch4.jpg";
+import c6 from "../../assets/images/hb_link1/blatch1.jpg";
+import c7 from "../../assets/images/hb_link1/blatch2.jpg";
+import c8 from "../../assets/images/hb_link1/blatch3.jpg";
+import c9 from "../../assets/images/hb_link1/blatch4.jpg";
 import c10 from "../../assets/pdf/Unlocking.pdf";
-import c11 from "../../assets/hb_link1/blatch5.jpg";
-import c12 from "../../assets/hb_link2/btm_bloomberg1.jpg";
-import c13 from "../../assets/hb_link2/btm_bloomberg2.jpg";
-import c14 from "../../assets/hb_link2/btm_bloomberg3.jpg";
-import c15 from "../../assets/hb_link6/bcoca1.jpg";
-import c16 from "../../assets/hb_link6/bcoca2.jpg";
-import c17 from "../../assets/hb_link6/bcoca3.jpg";
-import c18 from "../../assets/hb_link6/bcoca5.jpg";
-import c19 from "../../assets/hb_link6/bcoca6.jpg";
-import c20 from "../../assets/hb_link6/bcoca7.jpg";
-import c21 from "../../assets/hb_link7/b100_1.jpg";
-import c22 from "../../assets/hb_link7/b100_2.jpg";
-import c23 from "../../assets/hb_link7/b100_3.jpg";
-import c24 from "../../assets/hb_link7/b100_4.jpg";
-import c25 from "../../assets/hb_link7/b100_5.jpg";
-import c26 from "../../assets/hb_link7/b100_6.jpg";
-import c27 from "../../assets/hb_link7/b100_7.jpg";
-import c28 from "../../assets/hb_link7/b100_8.jpg";
-import c29 from "../../assets/hb_link7/b100_9.jpg";
-import c30 from "../../assets/hb_link13/bapple1.jpg";
-import c31 from "../../assets/hb_link17/bbald_1.jpg";
-import c32 from "../../assets/hb_link17/bbald_2.jpg";
-import c33 from "../../assets/hb_link17/bbald_3.jpg";
-import c34 from "../../assets/hb_link17/bbald_4.jpg";
-import c35 from "../../assets/hb_link18/bsoft_0.jpg";
-import c36 from "../../assets/hb_link18/bsoft_01.jpg";
-import c37 from "../../assets/hb_link18/bsoft_1.jpg";
-import c38 from "../../assets/hb_link18/bsoft_2.jpg";
-import c39 from "../../assets/hb_link18/bsoft_2.1.jpg";
-import c40 from "../../assets/hb_link18/bsoft_3.jpg";
-import c41 from "../../assets/hb_link3457/pic_1.mp4";
-import c42 from "../../assets/hb_link3457/pic_2.jpg";
-import c43 from "../../assets/hb_link3457/pic_3.jpg";
-import c44 from "../../assets/hb_link3457/pic_5.mp4";
-import c45 from "../../assets/hb_link3457/pic_14.jpg";
-import c46 from "../../assets/hb_link3457/pic_15.jpg";
-import c47 from "../../assets/hb_link3457/pic_16.jpg";
-import c48 from "../../assets/hb_link3457/pic_17.jpg";
-import c49 from "../../assets/hb_link3457/pic_18.jpg";
-import c50 from "../../assets/hb_link3457/pic_19.jpg";
-import c51 from "../../assets/hb_link3457/pic_20.jpg";
-import c52 from "../../assets/hb_link3457/pic_21.jpg";
-import c53 from "../../assets/hb_link3457/pic_22.jpg";
-import c54 from "../../assets/hb_link3457/pic_23.jpg";
-import c55 from "../../assets/hb_link3457/pic_24.jpg";
-import c56 from "../../assets/hb_link3457/pic_13.jpg";
-import c57 from "../../assets/hb_link3457/pic_12.jpg";
-import c58 from "../../assets/hb_link3457/pic_5.mp4";
-import c59 from "../../assets/hb_link3457/pic_7.jpg";
-import c60 from "../../assets/hb_link3457/pic_8.jpg";
-import c61 from "../../assets/hb_link3457/pic_9.jpg";
-import c62 from "../../assets/hb_link3457/pic_10.jpg";
-import c63 from "../../assets/hb_link3457/pic_11.jpg";
-import c64 from "../../assets/hb_link14/bski_1.jpg";
-import c65 from "../../assets/hb_link14/bski_2.jpg";
-import c66 from "../../assets/hb_link14/bski_3.jpg";
-import c67 from "../../assets/hb_link15/bpor_1.jpg";
-import c68 from "../../assets/hb_link15/bpor_2.jpg";
-import c69 from "../../assets/hb_link15/bpor_3.jpg";
-import c70 from "../../assets/hb_link15/bpor_4.jpg";
-import c71 from "../../assets/hb_link15/bpor_5.mp4";
-import c72 from "../../assets/hb_link16/bsnow_1.jpg";
-import c73 from "../../assets/hb_link16/bsnow_2.jpg";
-import c74 from "../../assets/hb_link16/bsnow_3.jpg";
-import c75 from "../../assets/hb_link16/bsnow_4.jpg";
+import c11 from "../../assets/images/hb_link1/blatch5.jpg";
+import c12 from "../../assets/images/hb_link2/btm_bloomberg1.jpg";
+import c13 from "../../assets/images/hb_link2/btm_bloomberg2.jpg";
+import c14 from "../../assets/images/hb_link2/btm_bloomberg3.jpg";
+import c15 from "../../assets/images/hb_link6/bcoca1.jpg";
+import c16 from "../../assets/images/hb_link6/bcoca2.jpg";
+import c17 from "../../assets/images/hb_link6/bcoca3.jpg";
+import c18 from "../../assets/images/hb_link6/bcoca5.jpg";
+import c19 from "../../assets/images/hb_link6/bcoca6.jpg";
+import c20 from "../../assets/images/hb_link6/bcoca7.jpg";
+import c21 from "../../assets/images/hb_link7/b100_1.jpg";
+import c22 from "../../assets/images/hb_link7/b100_2.jpg";
+import c23 from "../../assets/images/hb_link7/b100_3.jpg";
+import c24 from "../../assets/images/hb_link7/b100_4.jpg";
+import c25 from "../../assets/images/hb_link7/b100_5.jpg";
+import c26 from "../../assets/images/hb_link7/b100_6.jpg";
+import c27 from "../../assets/images/hb_link7/b100_7.jpg";
+import c28 from "../../assets/images/hb_link7/b100_8.jpg";
+import c29 from "../../assets/images/hb_link7/b100_9.jpg";
+import c30 from "../../assets/images/hb_link13/bapple1.jpg";
+import c31 from "../../assets/images/hb_link17/bbald_1.jpg";
+import c32 from "../../assets/images/hb_link17/bbald_2.jpg";
+import c33 from "../../assets/images/hb_link17/bbald_3.jpg";
+import c34 from "../../assets/images/hb_link17/bbald_4.jpg";
+import c35 from "../../assets/images/hb_link18/bsoft_0.jpg";
+import c36 from "../../assets/images/hb_link18/bsoft_01.jpg";
+import c37 from "../../assets/images/hb_link18/bsoft_1.jpg";
+import c38 from "../../assets/images/hb_link18/bsoft_2.jpg";
+import c39 from "../../assets/images/hb_link18/bsoft_2.1.jpg";
+import c40 from "../../assets/images/hb_link18/bsoft_3.jpg";
+import c41 from "../../assets/images/hb_link3457/pic_1.gif";
+import c42 from "../../assets/images/hb_link3457/pic_2.jpg";
+import c43 from "../../assets/images/hb_link3457/pic_3.jpg";
+import c44 from "../../assets/images/hb_link3457/pic_5.1.mp4";
+import c45 from "../../assets/images/hb_link3457/pic_14.jpg";
+import c46 from "../../assets/images/hb_link3457/pic_15.jpg";
+import c47 from "../../assets/images/hb_link3457/pic_16.jpg";
+import c48 from "../../assets/images/hb_link3457/pic_17.jpg";
+import c49 from "../../assets/images/hb_link3457/pic_18.jpg";
+import c50 from "../../assets/images/hb_link3457/pic_19.jpg";
+import c51 from "../../assets/images/hb_link3457/pic_20.jpg";
+import c52 from "../../assets/images/hb_link3457/pic_21.jpg";
+import c53 from "../../assets/images/hb_link3457/pic_22.jpg";
+import c54 from "../../assets/images/hb_link3457/pic_23.jpg";
+import c55 from "../../assets/images/hb_link3457/pic_24.jpg";
+import c56 from "../../assets/images/hb_link3457/pic_13.jpg";
+import c57 from "../../assets/images/hb_link3457/pic_12.jpg";
+import c58 from "../../assets/videos/a7.mp4";
+import c59 from "../../assets/images/hb_link3457/pic_7.jpg";
+import c60 from "../../assets/images/hb_link3457/pic_8.jpg";
+import c61 from "../../assets/images/hb_link3457/pic_9.jpg";
+import c62 from "../../assets/images/hb_link3457/pic_10.jpg";
+import c63 from "../../assets/images/hb_link3457/pic_11.jpg";
+import c64 from "../../assets/images/hb_link14/bski_1.jpg";
+import c65 from "../../assets/images/hb_link14/bski_2.jpg";
+import c66 from "../../assets/images/hb_link14/bski_3.jpg";
+import c67 from "../../assets/images/hb_link15/bpor_1.jpg";
+import c68 from "../../assets/images/hb_link15/bpor_2.jpg";
+import c69 from "../../assets/images/hb_link15/bpor_3.jpg";
+import c70 from "../../assets/images/hb_link15/bpor_4.jpg";
+import c71 from "../../assets/images/hb_link15/bpor_5.mp4";
+import c72 from "../../assets/images/hb_link16/bsnow_1.jpg";
+import c73 from "../../assets/images/hb_link16/bsnow_2.jpg";
+import c74 from "../../assets/images/hb_link16/bsnow_3.jpg";
+import c75 from "../../assets/images/hb_link16/bsnow_4.jpg";
+import c76 from "../../assets/images/hb_link3457/pic_4.mp4";
+import c77 from "../../assets/images/hb_link1/blatch1.1.jpg";
+import c78 from "../../assets/images/hb_link1/blatch1.2.jpg";
+import c79 from "../../assets/images/hb_link1/blatch6.mp4";
+import c80 from "../../assets/images/hb_link1/blatch1.3.jpg";
 
 // about component
 import a1 from "../../assets/images/a1.gif";
-import a2 from "../../assets/videos/a2.mp4";
+import a2 from "../../assets/images/a2.gif";
 import a5 from "../../assets/images/a5.jpg";
 import a6 from "../../assets/images/a6.jpg";
 import a7 from "../../assets/videos/a7.mp4";
 import a8 from "../../assets/pdf/annual.pdf";
 import a9 from "../../assets/pdf/domus.pdf";
 import a10 from "../../assets/pdf/casedeabitare.pdf";
-import ab1 from "../../assets/images/ab1.jpg";
-import ab2 from "../../assets/images/ab2.jpg";
-import ab3 from "../../assets/images/ab3.jpg";
 
-//shop component
+// shop component
+import s1 from "../../assets/images/9.gif";
+import s2 from "../../assets/images/shop/sh_2.jpg";
+import s3 from "../../assets/images/shop/sh_3.jpg";
+import s4 from "../../assets/images/shop/sh_4.jpg";
+import s4_1 from "../../assets/images/shop/sh_4.1.jpg";
+import s5 from "../../assets/images/shop/sh_5.jpg";
+import s6 from "../../assets/images/shop/sh_6.jpg";
+import s7 from "../../assets/images/shop/sh_7.jpg";
+import s8 from "../../assets/images/shop/sh_8.jpg";
+import s9 from "../../assets/images/shop/sh_9.jpg";
+import s10 from "../../assets/images/27.gif";
+import s11 from "../../assets/images/shop/sh_11.jpg";
+import s12 from "../../assets/images/shop/sh_12.jpg";
+import s13 from "../../assets/images/shop/sh_13.jpg";
+import s14 from "../../assets/images/shop/sh_14.jpg";
+import s15 from "../../assets/images/shop/sh_15.jpg";
+import s16 from "../../assets/images/shop/sh_16.jpg";
 
-import s1 from "../../assets/shop/sh_1.mp4";
-import s2 from "../../assets/shop/sh_2.jpg";
-import s3 from "../../assets/shop/sh_3.jpg";
-import s4 from "../../assets/shop/sh_4.jpg";
-import s4_1 from "../../assets/shop/sh_4.1.jpg";
-import s5 from "../../assets/shop/sh_5.jpg";
-import s6 from "../../assets/shop/sh_6.jpg";
-import s7 from "../../assets/shop/sh_7.jpg";
-import s8 from "../../assets/shop/sh_8.jpg";
-import s9 from "../../assets/shop/sh_9.jpg";
-import s10 from "../../assets/shop/sh_10.mp4";
-import s11 from "../../assets/shop/sh_11.jpg";
-import s12 from "../../assets/shop/sh_12.jpg";
-import s13 from "../../assets/shop/sh_13.jpg";
-import s14 from "../../assets/shop/sh_14.jpg";
-import s15 from "../../assets/shop/sh_15.jpg";
-import s16 from "../../assets/shop/sh_16.jpg";
-
-const about_files = [a1, a2, a5, a6, a7, a8, a9, a10, ab1, ab2, ab3];
+const about_files = [a1, a2, a5, a6, a7, a8, a9, a10];
 
 const home_files = [
   c00,
@@ -182,22 +210,10 @@ const home_files = [
     asset: c5,
     caption: "Latch",
   },
-  {
-    img: c6,
-    title: "Use of Latch Lens",
-  },
-  {
-    img: c7,
-    title: "Latch",
-  },
-  {
-    img: c8,
-    title: "Design",
-  },
-  {
-    img: c9,
-    title: "Collection",
-  },
+  c6,
+  c7,
+  c8,
+  c9,
   c10,
   c11,
   c12,
@@ -264,6 +280,11 @@ const home_files = [
   c73,
   c74,
   c75,
+  c76,
+  c77,
+  c78,
+  c79,
+  c80,
 ];
 
 const object_files = [
@@ -271,197 +292,263 @@ const object_files = [
     asset: h1,
     price: "$ 1.65 B startup",
     caption: "Latch",
+    type: "/archive-latch",
   },
   {
     asset: h2,
     price: "Not For Sale",
     caption: "Glass Sculpture",
+    type: "/archive-glass",
   },
   {
     asset: h3,
     price: "$ 15.00",
     caption: "Surf Magazine",
+    type: "/archive-surf",
   },
   {
     asset: h4,
     price: "$ 3.50",
     caption: "Coca-cola",
+    type: "/archive-coke",
   },
   {
     asset: h5,
     price: "Priceless",
     caption: "Coca-cola",
+    type: "/archive-coke",
   },
   {
     asset: h6,
     price: "Not available",
     caption: "Charge",
+    type: "/archive-latch",
   },
   {
     asset: h7,
     price: "$ 1900.00",
     caption: "Slip In",
+    type: "/archive-surf",
   },
   {
     asset: h8,
     price: "Prototype",
     caption: "Sound Box",
+    type: "/archive-sound",
   },
   {
     asset: h9,
     price: "$ 3600.00",
     caption: "Full Circle NFT",
+    type: "/archive-surf",
   },
   {
     asset: h10,
     price: "Sold Out",
     caption: "Glass Sculpture",
+    type: "/archive-glass",
   },
   {
     asset: h11,
     price: "Not Available",
     caption: "Logitech",
+    type: "/archive-powerring",
   },
   {
     asset: h12,
     price: "Not Available",
     caption: "Charge",
+    type: "/archive-charge",
   },
   {
     asset: h13,
     price: "Not just yet",
     caption: "2PRT. Tail",
+    type: "/archive-latch",
   },
   {
     asset: h14,
     price: "Not just yet",
-    caption: "2PRT. Tail",
+    caption: "2PRT. Front",
+    type: "/archive-surf",
   },
   {
     asset: h15,
     price: "Prototype",
     caption: "Porsche Shifter",
+    type: "/archive-porsche",
   },
   {
     asset: h16,
     price: "Prototype 2018",
     caption: "Logitech Charge Ring",
+    type: "/archive-powerring",
   },
   {
     asset: h17,
     price: "Check em out",
     caption: "Surfboards",
+    type: "/archive-surf",
   },
   {
     asset: h18,
     price: "Prototype",
     caption: "Charge",
+    type: "/archive-charge",
   },
   {
     asset: h19,
     price: "Priceless",
     caption: "Bloomberg for President",
+    type: "/archive-bloomberg",
   },
   {
     asset: h20,
     price: "$ 13.00",
     caption: "Outside Magazine",
+    type: "/outsidemagazine",
   },
   {
     asset: h21,
     price: "$ 12.00",
     caption: "Surfer Magazine",
+    type: "/archive-surf",
   },
   {
     asset: h22,
     price: "$ 300 - 500",
     caption: "Ski Goggles",
+    type: "/archive-skigoggles",
   },
   {
     asset: h23,
     price: "Sold Out",
     caption: "Flex Fin",
+    type: "/archive-surf",
   },
   {
     asset: h24,
     price: "Sold Out",
     caption: "Cappellini",
+    type: "/archive-cappellini",
   },
   {
     asset: h25,
     price: "$ 1800.00",
     caption: "Original",
+    type: "/archive-surf",
   },
   {
     asset: h26,
     price: "$ 13.00",
     caption: "Outside Magazine",
+    type: "/outsidemagazine",
   },
   {
     asset: h27,
     price: "$ 2200.00",
     caption: "Slip In",
+    type: "/archive-surf",
   },
   {
     asset: h28,
     price: "Out of Stock",
     caption: "Danger",
+    type: "/archive-danger",
   },
   {
     asset: h29,
     price: "$ 890.00",
     caption: "Neil Pryde Sail",
+    type: "/archive-neilpryde",
   },
   {
     asset: h30,
     price: "$ 420.00 (ebay)",
     caption: "Apple eMate",
+    type: "/archive-apple",
   },
   {
     asset: h31,
     price: "$ 25.00",
     caption: "Flex Box",
+    type: "/archive-surf",
   },
   {
     asset: h32,
     price: "For public use",
     caption: "Tork",
+    type: "/archive-tork",
   },
   {
     asset: h33,
     price: "$ 2100.00",
     caption: "Full Circle",
+    type: "/archive-surf",
   },
   {
     asset: h34,
     price: "$ 420.00",
     caption: "Scott Ski goggle",
+    type: "/archive-skigoggles",
   },
   {
     asset: h35,
     price: "Sold out",
     caption: "Neil Pryde Boom",
+    type: "/archive-neilpryde",
   },
   {
     asset: h36,
     price: "Not available anymore",
     caption: "Chumby",
+    type: "/archive-chumby",
   },
   {
     asset: h37,
     price: "$ Up cycled",
     caption: "Snowboard",
+    type: "/archive-snowboard",
   },
   {
     asset: h38,
     price: "$ 399.00 with software agreement",
     caption: "LATCH",
+    type: "/archive-surf",
   },
   {
     asset: h39,
     price: "Sold out",
     caption: "Clothing colab with Isaora",
+    type: "/archive-clothing",
   },
+  h40,
+  h41,
+  h42,
+  h43,
+  h44,
+  h45,
+  h46,
+  h47,
+  h48,
+  h49,
+  h50,
+  h51,
+  h52,
+  h53,
+  h54,
+  h55,
+  h56,
+  h57,
+  h58,
+  h59,
+  h60,
+  h61,
+  h62,
+  h63,
+  h64,
+  h65,
+  h66,
 ];
 
 const shop_files = [
